92b67f04b2f2de3b799b3912fdcbd2c81c6d5828
Alex Haiduc <haiduc.alx@gmail.com>
2022-07-15 21:00:53 +0300

    Implement sumAll function Function takes 2 integers And returns the sum of every number between(and including) them Set conditions for numbers to be integers higher than 0 Or else the output will be ERROR All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let sum = 0;
+    if(arg1 < 0 || arg2 < 0 || typeof arg1 !== "number" || typeof arg2 !== "number") {
+        sum = "ERROR";
+    } else if(arg1 < arg2){
+            for (let i = arg1; i <= arg2; i++) {
+                sum += i;            
+            }
+    } else if(arg1 > arg2) {
+        for (let i = arg2; i <= arg1; i++) {
+            sum += i;            
+        }        
+    }
+    return sum;
 };
 
+sumAll(1, 4) 
 // Do not edit below this line
 module.exports = sumAll;
