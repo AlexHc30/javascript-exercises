e27ce0d4a122b3029c71e66d24d150e9734b44db
Alex Haiduc <haiduc.alx@gmail.com>
2022-07-15 19:29:53 +0300

    Create function that returns reversed string Passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let reverse = '';
+    for(i = stringLength; i>=0; i--) {
+        reverse += string.charAt(i);
+    }
+    return reverse;
 };
 
+reverseString('hello there')
 // Do not edit below this line
 module.exports = reverseString;
