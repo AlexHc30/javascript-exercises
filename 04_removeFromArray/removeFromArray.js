383ef9381e8ddfc5772c922450fa7ef03c3459ca
Alex Haiduc <haiduc.alx@gmail.com>
2022-07-15 20:04:06 +0300

    Implement function The function takes an array and other arguments Then removes the other arguments from that array All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let arrayInput = Array.from(array);
+    for(let i = 1; i < arguments.length; i++) {
+        let remove = arguments[i];
+        for(let j = 0; j < arrayInput.length; j++) {
+            if(remove === arrayInput[j]) {
+                arrayInput.splice(j, 1);
+            }
+        }
+    }
+    return arrayInput;
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
