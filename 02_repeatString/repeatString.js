f287d8bfbd0a5171f90cf9c57c7de4bf53e1a29c
Alex Haiduc <haiduc.alx@gmail.com>
2022-07-15 19:11:49 +0300

    Create function that repeats string Created loop Added if condition that returns 'Error' if number of repeats<0 All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, maxNumber) {
+    let stringRepeated = "";
+    if(maxNumber < 0) {
+        stringRepeated = "ERROR";
+    } else {
+        for(i = 1; i <= maxNumber; i++) {
+            stringRepeated += string;
+        }
+    }
+    return stringRepeated;
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
