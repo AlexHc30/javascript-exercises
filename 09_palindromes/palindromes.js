f6690c68d2ab1b63a20bef864f054cb73b4f7bab
Alex Haiduc <haiduc.alx@gmail.com>
2022-07-18 15:44:54 +0300

    Write function that determines whether or not a given string is a palindrome All tests passed.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reverse = "";
+    initialString = string.toLowerCase().replace(/[^a-z]/g, '');
+    for (let i = string.length; i >= 0; i--) {
+        reverse += initialString.substr(i, 1); 
+    }
+    return initialString === reverse;
 };
 
 // Do not edit below this line
