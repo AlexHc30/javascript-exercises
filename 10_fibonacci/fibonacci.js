f5f1bdb8236e5b5e3ca05142b6491237540c59f2
Alex Haiduc <haiduc.alx@gmail.com>
2022-07-18 22:08:10 +0300

    Create function that returns a specific member of the Fibonacci sequence All tests passed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let fibSequence = [1, 1];
+    let fibonacciNo = 0;
+    if (member < 0) {
+        return "OOPS"
+    } else {
+        for (let i = 1; i < member - 1; i++) {
+            fibonacciNo = fibSequence[i-1] + fibSequence[i];
+            fibSequence.push(fibonacciNo);
+        }
+    }
+    return fibSequence[member-1];
 };
 
 // Do not edit below this line
