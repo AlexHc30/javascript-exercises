860767d15372812858f27932519e5b151d538504
Alex Haiduc <alex.haiduc01@gmail.com>
2022-08-04 16:18:56 +0300

    Return the oldest person from a given array of objects representing people with a birth and death year. Adapt function for instance where person doesn't have a death year. All tests passed.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce ((oldest, person) => {
+        if (!person.yearOfDeath) {
+            const d = new Date();
+            const currentYear = d.getFullYear();
+            person.age = currentYear - person.yearOfBirth;
+        } else {        
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return oldest.age > person.age ? oldest : person;
+    }, {})
 };
 
 // Do not edit below this line
