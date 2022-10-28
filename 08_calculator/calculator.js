41d09b03a378228414df5d0807af3f20a9256f89
Alex Haiduc <haiduc.alx@gmail.com>
2022-10-28 17:01:20 +0300

    Modify solution to several functions Use array.reduce instead of previous solution All tests passed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,26 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
+// const sum = function(array) {
+// 	let arraySum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     arraySum += array[i];
+//   }
+//   return arraySum;
+// };
 const sum = function(array) {
-	let arraySum = 0;
-  for (let i = 0; i < array.length; i++) {
-    arraySum += array[i];
-  }
-  return arraySum;
-};
+  return array.reduce((total, current) => total + current,0);
+}
 
+// const multiply = function(array) {
+//   let result = 1;
+//   for (let i = 0; i < array.length; i++) {
+//     result *= array[i];
+//   }
+//   return result;
+// };
 const multiply = function(array) {
-  let result = 1;
-  for (let i = 0; i < array.length; i++) {
-    result *= array[i];
-  }
-  return result;
+  return array.reduce((total, current) => total * current);
 };
 
 const power = function(a, b) {
